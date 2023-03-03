094bb42cf338914ff7e5910c9e7342e3f64f2339
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-02 23:01:29 -0300

    Added knex, express-async-errors, sqlite and sqlite3. Started to configurate db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
+require('express-async-errors')
+const database = require('./database/sqlite')
 const express = require('express')
-
 const routes = require('./routes')
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+database()
 
 const PORT = 3331
 
